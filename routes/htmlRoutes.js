02a8af7138a2b88f8a64b804bf3c9d30ba8681fe
JecoLantern <jbayonke@hotmail.com>
2018-12-01 21:43:28 -0800

    changed html, added search route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,12 +20,12 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/search", function(req, res) {
+    res.render("search");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
-  });
-
-  app.get("/search", function(req, res) {
-    res.render("search");
   });
 };
