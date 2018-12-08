a9cbb77403e3c37880626f1bec7bdc28067aad65
Retr0 <Ghozt24@gmail.com>
2018-12-08 13:09:00 -0800

    replaced the page footer with FABs containing modals

--- public/js/framework.js
+++ public/js/framework.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   $("select").formSelect();
 });
+
+//initialize modals and FABs
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'right',
+    hoverEnabled: false
+  });
+});
