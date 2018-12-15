49b453c7bd64d1c2cb106bcf44fd1f3e1a11027c
troy <gordontaaj@gmail.com>
2018-12-14 22:06:14 -0800

    fixing conflicts

--- routes/d3.js
+++ routes/d3.js
@@ -2,8 +2,6 @@ var cors = require('cors'),
     express = require('express'),
     populationRouter = express.Router(),        
     mysql = require('mysql');
-    var searchterm = require('./module')
-    var terms = searchterm.terms;
 var connection = mysql.createConnection({
  connectionLimit : 1, //important 
 'host' : 'localhost',
@@ -19,7 +17,7 @@ var getPopulations = function(){
     
     populationRouter.route('/')    
     .get(function(req,res){
-        connection.query('SELECT * from searches where search =',terms,  function(err, rows, fields) {
+        connection.query('SELECT * from searches',  function(err, rows, fields) {
             if (err) {
                 console.error(err);
                 res.statusCode = 500;
